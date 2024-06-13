a960fb49b68f4f161556f47ff49c2c6f7c530cbb
Moyasi Ginko <mrsiyam61@gmail.com>
2024-06-14 03:27:31 +0600

    Update VideoEmbed.tsx

--- web-ui/src/components/Featured/VideoEmbed.tsx
+++ web-ui/src/components/Featured/VideoEmbed.tsx
@@ -22,24 +22,6 @@ const VideoEmbed = () => {
           ></iframe>
         </div>
       </div>
-      <div
-        className="max-sm:h-20 hidden max-sm:block"
-        style={{ width: "400px", height: "200px" }}
-      >
-        <div style={{ width: "100%", height: "100%" }}>
-          <iframe
-            frameBorder="0"
-            allowFullScreen
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            title="MZ Media Launch Video"
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/GG2be1MKOik?autoplay=0&mute=0&controls=0&origin=https%3A%2F%2Fmzmedia.digital&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&enablejsapi=1&widgetid=3"
-            id="widget4"
-          ></iframe>
-        </div>
-      </div>
     </div>
   );
 };
