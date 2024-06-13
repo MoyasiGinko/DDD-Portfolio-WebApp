c22bfc1e2748e74bf916a275c5d2ef85a8cb8b3f
Moyasi Ginko <mrsiyam61@gmail.com>
2024-06-14 03:27:27 +0600

    Update TextFeature.tsx

--- web-ui/src/components/Featured/TextFeature.tsx
+++ web-ui/src/components/Featured/TextFeature.tsx
@@ -25,14 +25,14 @@ const TextParallaxContent = ({ children }) => {
       }}
     >
       <div className="relative h-[150vh]">
-        <StickyImage />
+        <StickyVideo />
       </div>
       {children}
     </div>
   );
 };
 
-const StickyImage = () => {
+const StickyVideo = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: targetRef,
@@ -55,33 +55,6 @@ const StickyImage = () => {
       className="sticky z-0 overflow-hidden rounded-3xl"
     >
       <VideoEmbed />
-    </motion.div>
-  );
-};
-
-const OverlayCopy = ({ subheading, heading }) => {
-  const targetRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-    offset: ["start end", "end start"],
-  });
-
-  const y = useTransform(scrollYProgress, [0, 1], [250, -250]);
-  const opacity = useTransform(scrollYProgress, [0.25, 0.5, 0.75], [0, 1, 0]);
-
-  return (
-    <motion.div
-      style={{
-        y,
-        opacity,
-      }}
-      ref={targetRef}
-      className="absolute left-0 top-0 flex h-screen w-full flex-col items-center justify-center text-white"
-    >
-      <p className="mb-2 text-center text-xl md:mb-4 md:text-3xl">
-        {subheading}
-      </p>
-      <p className="text-center text-4xl font-bold md:text-7xl">{heading}</p>
     </motion.div>
   );
 };
