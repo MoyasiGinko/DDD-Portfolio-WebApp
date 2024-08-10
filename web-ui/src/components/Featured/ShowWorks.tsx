bf85e143f485562a7273ee85b89f3e47caf5449c
Moyasi Ginko <mrsiyam61@gmail.com>
2024-08-11 04:51:34 +0600

    add good layout and ui

--- web-ui/src/components/Featured/ShowWorks.tsx
+++ web-ui/src/components/Featured/ShowWorks.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./project-showcase.css"; // Ensure this path is correct
 
 const ProjectShowcase: React.FC = () => {
